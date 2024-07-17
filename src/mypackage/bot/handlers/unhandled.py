81a8e022334adf0743bf669897b4d80e10f4efff
Andrii Hermak <hermak20102004@gmail.com>
2024-07-17 15:59:54 +0200

    Add .env file example

--- src/mypackage/bot/handlers/unhandled.py
+++ src/mypackage/bot/handlers/unhandled.py
@@ -3,8 +3,7 @@ from logging import Logger
 from telebot import TeleBot
 from telebot.types import Message, CallbackQuery
 
-from ...config.models import MessagesConfig
-
+from .. import Messages
 from ..utils import dummy_true, all_content_types
 
 
@@ -17,27 +16,25 @@ from ..utils import dummy_true, all_content_types
 def unhandled_messages_handler(
         message: Message,
         bot: TeleBot,
-        messages: MessagesConfig,
         logger: Logger,
         **kwargs):
     logger.debug(
         f"User {message.from_user.id} @{message.from_user.username} in chat {message.chat.id}"
         f"sent an unhandled message with content_type \"{message.content_type}\""
     )
-    bot.send_message(message.chat.id, messages.unknown_update)
+    bot.send_message(message.chat.id, Messages.unknown_update)
 
 
 def unhandled_callback_queries_handler(
         call: CallbackQuery,
         bot: TeleBot,
-        messages: MessagesConfig,
         logger: Logger,
         **kwargs):
     logger.debug(
         f"User {call.from_user.id} @{call.from_user.username} in chat {call.message.chat.id}"
         f"sent an unhandled callback query with callback_data \"{call.data}\""
     )
-    bot.send_message(call.message.chat.id, messages.unknown_update)
+    bot.send_message(call.message.chat.id, Messages.unknown_update)
 
 
 def register_handlers(bot: TeleBot):
